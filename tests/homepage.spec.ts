7d2d1dc022b20255fb359fd040cbeb3bca7b208c
Ana Paula <ap.almeidapm@gmail.com>
2025-09-20 21:13:26 +0100

    add teste do input

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -6,3 +6,21 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/e-cordel/);
 });
+
+test('input test', async ({ page }) => {
+  await page.goto('https://ler.ecordel.com.br');
+
+  //selecionar o input
+  const input = page.locator('input[placeholder="Pesquisar cordel"]')
+
+  //Digita no input
+  await input.fill("Oração da Menina Benigna")
+
+  const title = page.locator('span', {
+    hasText: 'Oração da Menina Benigna'
+  })
+
+  await expect(title).toBeVisible()
+  
+
+});
